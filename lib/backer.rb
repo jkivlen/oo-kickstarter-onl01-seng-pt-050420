67bd739f8ba075bf4ab6f0e2619abd411f2c9476
Jimmy Kivlen <jimmykivlen@gmail.com>
2020-06-12 18:14:28 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -7,8 +7,9 @@ class Backer
     @backed_projects = []
   end
   
-  def back_project
-    Project.new = @backed_projects
+  def back_project(project)
+    @backed_projects << project
+    
   end
   
 end
